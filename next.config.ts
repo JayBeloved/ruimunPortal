af66093b8661e70548a011baa4e3bf750601e03e
John J. Lawal <contact.johnjlawal@gmail.com>
2025-12-20 17:03:35 +0000

    First let's replace the placeholder images and edit the next.config.ts t

--- next.config.ts
+++ next.config.ts
@@ -28,6 +28,24 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'drive.google.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'imgur.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.imgur.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
