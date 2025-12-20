d06ae66b8a4adbd5033becc579caae57df6b3356
John J. Lawal <contact.johnjlawal@gmail.com>
2025-12-20 17:45:57 +0000

    Let's scale the logo image in the header, it's too small

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 export function Logo({ className }: { className?: string }) {
   return (
     <Link href="/" className={cn("flex items-center gap-2", className)}>
-      <Image src="https://i.imgur.com/xZVq78X.png" alt="RUIMUN Logo" width={40} height={40} className="h-8 w-auto" />
+      <Image src="https://i.imgur.com/xZVq78X.png" alt="RUIMUN Logo" width={50} height={50} className="h-10 w-auto" />
       <span className="font-headline font-bold text-xl tracking-tight text-primary">
         RUIMUN &apos;26
       </span>
