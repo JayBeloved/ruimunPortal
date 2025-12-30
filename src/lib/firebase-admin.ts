195d53aaa54bf6528469cf5d4940dad6a0624868
John J. Lawal <contact.johnjlawal@gmail.com>
2025-12-30 08:17:32 +0000

    Refactor firebase-admin to use environment variables

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -2,12 +2,26 @@ import * as admin from 'firebase-admin';
 
 if (!admin.apps.length) {
   try {
-    const serviceAccount = require('@/lib/serviceAccountKey.json');
+    // Check if the necessary environment variables are set
+    if (
+      !process.env.FIREBASE_PROJECT_ID ||
+      !process.env.FIREBASE_CLIENT_EMAIL ||
+      !process.env.FIREBASE_PRIVATE_KEY
+    ) {
+      throw new Error('Missing Firebase Admin SDK credentials in environment variables.');
+    }
+
     admin.initializeApp({
-        credential: admin.credential.cert(serviceAccount)
+      credential: admin.credential.cert({
+        projectId: process.env.FIREBASE_PROJECT_ID,
+        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+        // The private key must be properly formatted
+        privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\n/g, '\n'),
+      }),
     });
+    console.log('Firebase Admin SDK initialized successfully.');
   } catch (error) {
-    console.error('Firebase admin initialization error', error);
+    console.error('Firebase admin initialization error:', error);
   }
 }
 
